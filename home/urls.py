a253f5576d6a7d1c3303c24807963d4c1dc40d5c
MuhammadOsama320 <usamaakram320@gmail.com>
2021-04-10 13:48:44 +0500

    Crud Operation Task Complete.

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,9 @@ from home import views
 urlpatterns = [
     path('', views.home),
     path('show', views.show),
-    path('send', views.send)
+    path('send', views.send),
+    path('delete', views.delete),
+    path('edit', views.edit),
+    path('RecordEdited', views.RecordEdited)
+
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from home.models import Entry
 
 
@@ -22,5 +22,32 @@ def send(request):
         msg = "Data Stored Successfully"
         return render(request, "home.html", {'msg': msg})
 
+    else:
+        return HttpResponse("<h1> 404 Error</h1>")
+
+
+def delete(request):
+    ID = request.GET['id']
+    Entry.objects.filter(ID=ID).delete()
+    return HttpResponseRedirect('show')
+
+
+def edit(request):
+    ID = request.GET['id']
+    data1 = data2 = "Not_Available"
+    for data in Entry.objects.filter(ID=ID):
+        data1 = data.data1
+        data2 = data.data2
+
+    return render(request, "edit.html", {'ID': ID, 'data1': data1, 'data2': data2})
+
+
+def RecordEdited(request):
+    if request.method == 'POST':
+        ID = request.POST['id']
+        data1 = request.POST['data1']
+        data2 = request.POST['data2']
+        Entry.objects.filter(ID=ID).update(data1=data2, data2=data2)
+        return HttpResponseRedirect("show")
     else:
         return HttpResponse("<h1> 404 Error</h1>")
